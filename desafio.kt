9b1457f691f4a82ecc9a6aba9989f6bcee0a795a
kahbueno <130587403+kahbueno@users.noreply.github.com>
2024-01-30 12:21:18 -0300

    Desafio Resolvido

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Aluno(val name: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int, var nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+    fun matricular(vararg alunos: Aluno){
+    for (aluno in alunos) {
+        inscritos.add(aluno)
+    }}
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução", 120, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Orientações a Objetos", 180, Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Funções e Tratamento de Exceções", 240, Nivel.AVANÇADO)
+    val conteudos = listOf(conteudo1, conteudo2, conteudo3)
+    
+     val formacao = Formacao("Linguagem de Programção Kotlin", conteudos)
+    
+    val aluno1 = Aluno("Karenn")
+    val aluno2 = Aluno("Falvo Jr")
+    val aluno3 = Aluno("DIO")
+    formacao.matricular(aluno1, aluno2, aluno3)
+    
+    println("Informações sobre a formação ${formacao.nome}:")
+    for (conteudo in formacao.conteudos){
+        println(conteudo.toString())
+    }
+    println()
+    println("Inscritos:")
+    for (inscrito in formacao.inscritos){
+        println(inscrito.name)
+    }
+    
 }
